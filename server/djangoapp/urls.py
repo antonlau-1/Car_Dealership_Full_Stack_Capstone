6e30644c4195fc5fde4b42014bc46af3ee42929b
antonlau-1 <antony.hy.lau@gmail.com>
2024-09-21 22:49:20 +0100

    dealer review microservices views

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,11 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='registration'),
     path('get_cars/', views.get_cars, name='getcars'),
+    path('get_dealers', views.get_dealerships, name='getdealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='getdealersbystate'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='getdealer'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealerreview'),
+    path('add_review', views.add_review, name='addreview'),
     
 
     # path for dealer reviews view
